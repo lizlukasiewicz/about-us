2fd640bf02bd57444600b144ec39e616cde4d8f6
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2021-06-29 17:25:09 -0400

    bootsrap navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
 import Nav from './Components/Nav'
-import Welcome from './Components/Welcome'
-import People from './Components/People'
-import Footer from './Components/Footer'
+//import Welcome from './Components/Welcome'
+//import People from './Components/People'
+//import Footer from './Components/Footer'
 
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <Welcome />
-      <People />
-      <Footer />
+
     </div>
   );
 }
