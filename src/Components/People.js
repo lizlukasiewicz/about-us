849b4121ab4061e577ea89676b4c7d6c1f9038d5
erikwinkel <erikpwinkel@gmail.com>
2021-06-29 14:29:54 -0700

    add People component

---
+++ src/Components/People.js
@@ -0,0 +1,29 @@
+import { useState } from "react"
+// import Person from './Components/Person'
+import people from '../data/peopleData'
+
+export default function People() {
+    const [search,setSearch] = useState('')
+    const [renderedPeople,setRenderedPeople] = useState(people)
+
+    const handleSearch = (e) => {
+        let newSearch = e.target.value
+        setSearch(newSearch)
+        if(newSearch){
+            let foundPeople = people.map((person) => {
+                return person.name.toLowerCase().includes(newSearch.toLowerCase())
+            })
+            setRenderedPeople(foundPeople)
+        }
+    }
+
+    return (
+        <div>
+            <form>
+                <label for="nameSearch">Search by name</label>
+                <input type="text" id="nameSearch" value={search} onChange={handleSearch}></input>
+            </form>
+            <Person people={renderedPeople} />
+        </div>
+    )
+}
