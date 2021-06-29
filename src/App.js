63766010b155803e46a1e1171531a57be886c850
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2021-06-29 16:58:48 -0400

    barebones navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
+      <Nav />
       {
         //stuff goes here
       }
+
     </div>
   );
 }
